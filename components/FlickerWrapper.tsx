b94e83367469e1b5d5bebab4a60161db807a0a70
Parker Bedlan <parkerbedlan@gmail.com>
2022-04-15 02:31:25 -0500

    combined the blitz-inspired flicker wrapper with my flicker suppressor

--- components/FlickerWrapper.tsx
+++ components/FlickerWrapper.tsx
@@ -1,5 +1,8 @@
+import { Loader } from "@mantine/core";
+import { useScrollLock } from "@mantine/hooks";
 import Head from "next/head";
 import React, { ReactNode, useEffect } from "react";
+import { useIsPageRendered } from "../hooks/useIsPageRendered";
 
 type FlickerWrapperProps = {
   children: ReactNode;
@@ -13,7 +16,8 @@ export const FlickerWrapper: React.FC<FlickerWrapperProps> = ({ children }) => {
   }, []);
   return (
     <>
-      {<NoPageFlicker />}
+      <NoPageFlicker />
+      <FirstRenderFlickerSuppressor />
       {children}
     </>
   );
@@ -50,5 +54,21 @@ export const NoPageFlicker = () => {
         <style dangerouslySetInnerHTML={{ __html: noscriptCSS }} />
       </noscript>
     </Head>
+  );
+};
+
+const FirstRenderFlickerSuppressor: React.FC<{}> = ({}) => {
+  const [scrollLocked, setScrollLocked] = useScrollLock(true);
+  useEffect(() => {
+    setScrollLocked(false);
+  }, [setScrollLocked]);
+
+  const rendered = useIsPageRendered();
+
+  if (rendered) return null;
+  return (
+    <div className="absolute h-screen w-screen bg-gray-800 z-50 flex justify-center items-center">
+      <Loader />
+    </div>
   );
 };
