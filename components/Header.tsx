6fab7cb30db71c3e6ee1fd33ad553a8800906e39
Parker Bedlan <parkerbedlan@gmail.com>
2022-06-07 22:30:46 -0500

    no dropdown transition

--- components/Header.tsx
+++ components/Header.tsx
@@ -119,9 +119,9 @@ const NavLink: React.FC<Link> = ({ text, href, dropdownItems }) => {
       </div>
       {dropdownItems && (
         <div
-          className={`absolute -z-50 top-full flex flex-col space-y-3 mt-3 bg-black w-max transition-transform duration-75 ${
+          className={`absolute -z-50 top-full flex flex-col space-y-3 mt-3 bg-black w-max ${
             hovered ? "-translate-y-3" : `-translate-y-[100vh]`
-          }`}
+          }`} //transition-transform
         >
           {dropdownItems.map((item) => (
             <div key={item.href}>
